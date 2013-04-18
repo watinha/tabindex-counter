cbb6524f3a8e8933b158704d24cd7fd0a07897de
Willian Massami Watanabe <watanabe_willian@yahoo.com>
2013-04-18 18:34:58 -0300

    now our script counts the number of calls to addEventListener method

--- content_scripts/ElementCounter.js
+++ content_scripts/ElementCounter.js
@@ -1,4 +1,20 @@
-var ElementCounter = (function () {
+window.ElementCounter = (function () {
+	var get_listeners_callees,
+        get_onevents_elements,
+        get_tabindex_elements,
+		trueAddEventListener,
+		elements_with_listeners = [];
+
+	trueAddEventListener = HTMLElement.prototype.addEventListener;
+	HTMLElement.prototype.addEventListener = function () {
+		elements_with_listeners.push(this);
+		trueAddEventListener.apply(this, arguments);
+	};
+
+	get_listeners_callees = function () {
+		return elements_with_listeners.length;
+	};
+
     return {
         get_listeners_callees: get_listeners_callees,
         get_onevents_elements: get_onevents_elements,
--- tests/content_scripts/test_ElementCounter.js
+++ tests/content_scripts/test_ElementCounter.js
@@ -3,12 +3,31 @@
 			clientScripts: ["content_scripts/ElementCounter.js"]
 		});
 	casper.start("tests/fixtures/dropdown.html", function () {
-		this.test.comment("Sanity check for tests infra-structure on: dropdown.html");
-		this.test.assertTitle("Exemplo menu", "The page title should be correct...");
+		var self = this;
+		(function () {
+			self.test.comment("Sanity check for tests infra-structure on: dropdown.html");
+			self.test.assertTitle("Exemplo menu", "The page title should be correct...");
+		}());
+
+		(function () {
+			self.test.comment("Testing get_listeners_callees should return the number" +
+							  " of calls to the addEventListener method");
+			self.test.assertEval(function () {
+				return (ElementCounter.get_listeners_callees() == 5);
+			}, "there should be 5 calls to the addEventListener function");
+		}());
+
+		//(function () {
+		//	self.test.comment("Testing get_onevents_elements should return the number" +
+		//					  " of ON events attributes in DOM Nodes");
+		//	self.test.assertEval(function () {
+		//		return (ElementCounter.get_onevents_elements() == 5);
+		//	}, "there should be 5 DOM Nodes with onevents set");
+		//}());
 	});
 
 	casper.run(function () {
-		this.test.done(1);
+		this.test.done(2);
 		this.test.renderResults(true);
 	});
 }());
