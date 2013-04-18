a96d6d8b6b25d4542864d06609d658556f1638f3
Willian Massami Watanabe <watanabe_willian@yahoo.com>
2013-04-18 14:44:41 -0300

    improving the test setup

--- tests/content_scripts/test_ElementCounter.js
+++ tests/content_scripts/test_ElementCounter.js
@@ -1,5 +1,14 @@
 (function () {
-	var casper = require("casper").create();
-	casper.echo("First test to be made here :)");
-	casper.exit(0);
+	var casper = require("casper").create({
+			clientScripts: ["content_scripts/ElementCounter.js"]
+		});
+	casper.start("tests/fixtures/dropdown.html", function () {
+		this.test.comment("Sanity check for tests infra-structure on: dropdown.html");
+		this.test.assertTitle("Exemplo menu", "The page title should be correct...");
+	});
+
+	casper.run(function () {
+		this.test.done(1);
+		this.test.renderResults(true);
+	});
 }());
