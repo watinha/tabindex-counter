1b37fcf09f1fec3b72a15be6f8458ac71fad989b
Willian Massami Watanabe <watanabe_willian@yahoo.com>
2013-04-18 14:01:04 -0300

    first dummy files with the content scripts to be inserted via inject js file in casperjs

---
+++ content_scripts/ElementCounter.js
@@ -0,0 +1,7 @@
+var ElementCounter = (function () {
+    return {
+        get_listeners_callees: get_listeners_callees,
+        get_onevents_elements: get_onevents_elements,
+        get_tabindex_elements: get_tabindex_elements
+    };
+}());
