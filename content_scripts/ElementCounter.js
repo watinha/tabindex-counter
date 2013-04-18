af624cc8a68f70c88023d98a514aa0a54522a2c8
Willian Massami Watanabe <watanabe_willian@yahoo.com>
2013-04-18 19:54:05 -0300

    now checking the mouse events set inline

--- content_scripts/ElementCounter.js
+++ content_scripts/ElementCounter.js
@@ -1,19 +1,44 @@
 window.ElementCounter = (function () {
-	var get_listeners_callees,
+    var get_listeners_callees,
         get_onevents_elements,
         get_tabindex_elements,
-		trueAddEventListener,
-		elements_with_listeners = [];
+        trueAddEventListener,
+        elements_with_listeners = [];
 
-	trueAddEventListener = HTMLElement.prototype.addEventListener;
-	HTMLElement.prototype.addEventListener = function () {
-		elements_with_listeners.push(this);
-		trueAddEventListener.apply(this, arguments);
-	};
+    trueAddEventListener = HTMLElement.prototype.addEventListener;
+    HTMLElement.prototype.addEventListener = function () {
+        elements_with_listeners.push(this);
+        trueAddEventListener.apply(this, arguments);
+    };
 
-	get_listeners_callees = function () {
-		return elements_with_listeners.length;
-	};
+    get_listeners_callees = function () {
+        return elements_with_listeners.length;
+    };
+
+    /*
+     * Considering only the mouse events:
+     *  onclick ondblclick onmousedown onmousemove
+     *  onmouseout onmouseover onmouseup onmouseleave
+     */
+    get_onevents_elements = function () {
+        var all_nodes = document.getElementsByTagName("*"),
+            supported_events = [
+                "onclick", "ondblclick", "onmousedown", "onmousemove",
+                "onmouseout", "onmouseover", "onmouseup", "onmouseleave"
+            ],
+            nodes_with_event = [];
+
+        for (var i = 0; i < all_nodes.length; i++) {
+            if (all_nodes[i].nodeType == 1) {
+                for (var j = 0; j < supported_events.length; j++) {
+                    if (all_nodes[i][supported_events[j]])
+                        nodes_with_event.push(all_nodes[i][supported_events[j]]);
+                };
+            }
+        };
+
+        return nodes_with_event.length;
+    };
 
     return {
         get_listeners_callees: get_listeners_callees,
--- tests/content_scripts/test_ElementCounter.js
+++ tests/content_scripts/test_ElementCounter.js
@@ -1,33 +1,38 @@
 (function () {
-	var casper = require("casper").create({
-			clientScripts: ["content_scripts/ElementCounter.js"]
-		});
-	casper.start("tests/fixtures/dropdown.html", function () {
-		var self = this;
-		(function () {
-			self.test.comment("Sanity check for tests infra-structure on: dropdown.html");
-			self.test.assertTitle("Exemplo menu", "The page title should be correct...");
-		}());
+    var casper = require("casper").create({
+            clientScripts: ["content_scripts/ElementCounter.js"]
+        });
+    casper.start("tests/fixtures/dropdown.html", function () {
+        var self = this;
+        (function () {
+            self.test.comment("Sanity check for tests infra-structure on: dropdown.html");
+            self.test.assertTitle("Exemplo menu", "The page title should be correct...");
+        }());
 
-		(function () {
-			self.test.comment("Testing get_listeners_callees should return the number" +
-							  " of calls to the addEventListener method");
-			self.test.assertEval(function () {
-				return (ElementCounter.get_listeners_callees() == 5);
-			}, "there should be 5 calls to the addEventListener function");
-		}());
+        (function () {
+            var result;
+            self.test.comment("Testing get_listeners_callees should return the number" +
+                              " of calls to the addEventListener method");
+            result = self.evaluate(function () {
+                return (ElementCounter.get_listeners_callees());
+            });
+            self.test.assertEquals(result, 5, "there should be 5 calls to the addEventListener function");
+        }());
 
-		//(function () {
-		//	self.test.comment("Testing get_onevents_elements should return the number" +
-		//					  " of ON events attributes in DOM Nodes");
-		//	self.test.assertEval(function () {
-		//		return (ElementCounter.get_onevents_elements() == 5);
-		//	}, "there should be 5 DOM Nodes with onevents set");
-		//}());
-	});
+        (function () {
+            var result;
+            self.test.comment("Testing get_onevents_elements should return the number" +
+                              " of ON events attributes in DOM Nodes");
+            result = self.evaluate(function () {
+                var result = (ElementCounter.get_onevents_elements());
+                return result;
+            });
+            self.test.assertEquals(result, 5, "there should be 5 DOM Nodes with onevents set");
+        }());
+    });
 
-	casper.run(function () {
-		this.test.done(2);
-		this.test.renderResults(true);
-	});
+    casper.run(function () {
+        this.test.done(3);
+        this.test.renderResults(true);
+    });
 }());
