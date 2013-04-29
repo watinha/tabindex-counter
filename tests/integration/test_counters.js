bde19a39df93c2de791905c3a0c95c6f20e3f085
Willian Massami Watanabe <watanabe_willian@yahoo.com>
2013-04-29 18:59:49 -0300

    one more integration tests

--- tests/integration/test_counters.js
+++ tests/integration/test_counters.js
@@ -54,10 +54,27 @@
             self.test.assert( ! result[5], "ListenerElements: 5 there should be only one tabindexed");
         }());
 
+        (function () {
+            var result = self.evaluate(function () {
+                var elements = (ListenerElements.get_elements()),
+                    result = [];
+                for (var i = 0; i < elements.length; i++) {
+                    result.push(TabIndexElements.has_role(elements[i]));
+                };
+                return result;
+            });
+            self.test.assert(result[0], "ListenerElements: 0 there should be only one role");
+            self.test.assert( ! result[1], "ListenerElements: 1 there should be only one role");
+            self.test.assert( ! result[2], "ListenerElements: 2 there should be only one role");
+            self.test.assert( ! result[3], "ListenerElements: 3 there should be only one role");
+            self.test.assert( ! result[4], "ListenerElements: 4 there should be only one role");
+            self.test.assert( ! result[5], "ListenerElements: 5 there should be only one role");
+        }());
+
     });
 
     casper.run(function () {
-        this.test.done(14);
+        this.test.done(20);
     });
 }(casper));
 
