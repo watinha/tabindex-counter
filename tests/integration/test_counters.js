53f28386a84d0df92178b750fd07528fa79fc5f3
Willian Massami Watanabe <watanabe_willian@yahoo.com>
2013-04-29 17:41:27 -0300

    one more integration just to be a little safier

--- tests/integration/test_counters.js
+++ tests/integration/test_counters.js
@@ -22,10 +22,25 @@
             self.test.assert( ! result[3], "3 there should be only one tabindexed");
         }());
 
+        (function () {
+            var result = self.evaluate(function () {
+                var elements = (OnEventElements.get_elements()),
+                    result = [];
+                for (var i = 0; i < elements.length; i++) {
+                    result.push(TabIndexElements.has_role(elements[i]));
+                };
+                return result;
+            });
+            self.test.assert( ! result[0], "there should be only one with role");
+            self.test.assert( ! result[1], "1 there should be only one role");
+            self.test.assert( ! result[2], "2 there should be only one role");
+            self.test.assert(result[3], "3 there should be only one role");
+        }());
+
     });
 
     casper.run(function () {
-        this.test.done(4);
+        this.test.done(8);
     });
 }(casper));
 
