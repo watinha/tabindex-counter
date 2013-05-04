741a06e64cf00f97b98c13c384f6213256eac374
Willian Massami Watanabe <watanabe_willian@yahoo.com>
2013-05-04 20:12:06 -0300

    just a little refactoring

--- main.js
+++ main.js
@@ -25,37 +25,42 @@ casper.start(url, function () {
     }));
 
     this.wait(2000, function () {
-        // first statistics
-        this.echo("Number of JS Listeners:             " + this.evaluate(function () {
-            return ListenerElements.get_number();
-        }));
-        this.echo("Number of on event functions:       " + this.evaluate(function () {
-            return OnEventElements.get_number();
-        }));
-        this.echo("Number of tabindexed elements:      " + this.evaluate(function () {
-            return TabIndexElements.get_number();
-        }));
-        this.echo("Number of elements with roles:      " + this.evaluate(function () {
-            return RoleElements.get_number();
-        }));
-
-        // measuring how js listeners are set in the tabindex sequence
-        this.echo("Percentage of tabindexed listeners: " + this.evaluate(function () {
-            var listener_elements = ListenerElements.get_elements(),
-                onevent_elements = OnEventElements.get_elements(),
-                tabindexed_elements = 0.0;
-            for (var i = 0; i < listener_elements.length; i++) {
-                if (TabIndexElements.is_tabindexed(listener_elements[i]) ||
-                    RoleElements.has_role(listener_elements[i]))
-                    tabindexed_elements++;
-            };
-            for (var i = 0; i < onevent_elements.length; i++) {
-                if (TabIndexElements.is_tabindexed(onevent_elements[i]) ||
-                    RoleElements.has_role(onevent_elements[i]))
-                    tabindexed_elements++;
-            };
-            return (tabindexed_elements / (listener_elements.length + onevent_elements.length)) * 100 + "%";
-        }));
+        var json_data = {
+            listeners_number: this.evaluate(function () {
+                return ListenerElements.get_number();
+            }),
+            onevents_number: this.evaluate(function () {
+                return OnEventElements.get_number();
+            }),
+            tabindexed_number: this.evaluate(function () {
+                return TabIndexElements.get_number();
+            }),
+            roles_number: this.evaluate(function () {
+                return RoleElements.get_number();
+            }),
+            percentage: this.evaluate(function () {
+                var listener_elements = ListenerElements.get_elements(),
+                    onevent_elements = OnEventElements.get_elements(),
+                    tabindexed_elements = 0.0;
+                for (var i = 0; i < listener_elements.length; i++) {
+                    if (TabIndexElements.is_tabindexed(listener_elements[i]) ||
+                        RoleElements.has_role(listener_elements[i]))
+                        tabindexed_elements++;
+                };
+                for (var i = 0; i < onevent_elements.length; i++) {
+                    if (TabIndexElements.is_tabindexed(onevent_elements[i]) ||
+                        RoleElements.has_role(onevent_elements[i]))
+                        tabindexed_elements++;
+                };
+                return (tabindexed_elements / (listener_elements.length + onevent_elements.length)) * 100 + "%";
+            })
+        };
+        this.echo(
+            ["Number of JS Listeners:             " + json_data.listeners_number,
+             "Number of on event functions:       " + json_data.onevents_number,
+             "Number of tabindexed elements:      " + json_data.tabindexed_number,
+             "Number of elements with roles:      " + json_data.roles_number,
+             "Percentage of tabindexed listeners: " + json_data.percentage].join("\n"));
     });
 });
 
