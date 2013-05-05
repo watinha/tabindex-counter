d972e639e77480203a43076848f6656e06230f1a
Willian Massami Watanabe <watanabe_willian@yahoo.com>
2013-05-04 23:22:22 -0300

    now with a little test.data

--- utils/run_multiple.sh
+++ utils/run_multiple.sh
@@ -3,5 +3,5 @@
 WEBSITES=`cat $1`
 
 for WEBSITE in $WEBSITES; do
-    casperjs ../main.js $WEBSITE
+    casperjs main.js $WEBSITE --output=csv | grep -v "Checking URL" | grep -v "Page Title" | grep -v "Done." >> $2
 done
